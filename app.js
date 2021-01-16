51bac6730f461531c44f5a5d1044a024d6f6fea9
Rubesh K R <rupesh.maxpain@gmail.com>
2021-01-16 18:15:22 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+
+//Movement Animation to happen
+const card = document.querySelector('.card');
+const container = document.querySelector('.container');
+
+//Items
+const title = document.querySelector(".title");
+const sneaker = document.querySelector(".sneaker img");
+const purchase = document.querySelector(".purchase button");
+const description = document.querySelector(".info h3");
+const sizes = document.querySelector(".sizes");
+
+
+
+//Moving Animation Event
+container.addEventListener('mousemove', (e) =>{
+    let xAxis = (window.innerWidth / 2 - e.pageX) /25;
+    let yAxis = (window.innerHeight / 2 -e.pageY) /25;
+    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`; 
+});
+
+//Animate In 
+container.addEventListener("mouseenter", (e) => {
+    card.style.transition = "none";
+    //pop In
+    title.style.transform = "translateZ(150px)";
+    purchase.style.transform = "translateZ(75px)";
+    description.style.transform = "translateZ(125px)";
+    sizes.style.transform = "translateZ(100px)";
+    sneaker.style.transform = "translateZ(200px) rotate(-45deg)";
+});
+
+
+//Animate Out
+container.addEventListener("mouseleave", (e) => {
+    card.style.transition = "all 0.5s ease";
+    card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+    //pop out
+    title.style.transform = "translateZ(0px)";
+    purchase.style.transform = "translateZ(0px)";
+    description.style.transform = "translateZ(0px)";
+    sizes.style.transform = "translateZ(0px)";
+    sneaker.style.transform = "translateZ(0px) rotate(0deg)";
+});
